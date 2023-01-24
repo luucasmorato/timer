ec51bc3f09535b8f6f0d8f660a0819342a2ff050
Lucas Morato <luucasmorato@gmail.com>
2023-01-23 22:01:18 -0300

    configure application theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
 import { defaultTheme } from "./styles/themes/default";
 
@@ -6,6 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div className="App">Hello World</div>;
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
