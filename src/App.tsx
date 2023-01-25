f18dd0a33b3cb4b605f172a6d97aaf48bcd035bf
Lucas Morato <luucasmorato@gmail.com>
2023-01-24 22:28:42 -0300

    configure routes home and history

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 
 import { defaultTheme } from './styles/themes/default'
@@ -6,7 +8,9 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">Hello World</div>;
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
